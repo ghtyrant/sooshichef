5ede20463cb29836f33456482a1cb93a451b04c6
Fabian Schlager <fabian.schlager@skyr.at>
2016-06-28 18:11:34 +0200

    Allow changing of sampling settings

--- Makefile
+++ Makefile
@@ -5,8 +5,8 @@ LD := $(CC)
 GLIB_CFLAGS  := $(shell pkg-config --cflags gtk+-3.0)
 GLIB_LDFLAGS := $(shell pkg-config --libs gtk+-3.0)
 
-CFLAGS  := -Wall -pedantic -g $(GLIB_CFLAGS) -I../sooshi/src/
-LDFLAGS := $(GLIB_LDFLAGS) -L../sooshi/ -lsooshi
+CFLAGS  := -Wall -pedantic -g $(GLIB_CFLAGS) -I../libsooshi/src/
+LDFLAGS := $(GLIB_LDFLAGS) -L../libsooshi/ -lsooshi
 
 TARGET  := sooshichef
 SOURCES := $(wildcard src/*.c)
@@ -21,10 +21,13 @@ $(TARGET): $(OBJECTS)
 	$(CC) $(CFLAGS) -c $^ -o $@
 
 run: $(TARGET)
-	LD_LIBRARY_PATH=/home/fabian/projects/privat/sooshi/ G_MESSAGES_DEBUG=all ./$(TARGET)
+	LD_LIBRARY_PATH=../libsooshi/ G_MESSAGES_DEBUG=all ./$(TARGET)
 
 gdb: $(TARGET)
-	LD_LIBRARY_PATH=/home/fabian/projects/privat/sooshi/ G_MESSAGES_DEBUG=all gdb ./$(TARGET)
+	LD_LIBRARY_PATH=../libsooshi/ G_MESSAGES_DEBUG=all gdb ./$(TARGET)
+
+valgrind: all
+	LD_LIBRARY_PATH=../libsooshi/ valgrind --tool=memcheck --leak-check=full --show-leak-kinds=definite,indirect,possible --show-reachable=no --leak-resolution=high --num-callers=20 --trace-children=no --child-silent-after-fork=yes --track-fds=yes --track-origins=yes ./$(TARGET) 2>&1 | tee valgrind.log
 
 clean:
 	rm $(TARGET) $(OBJECTS)
--- src/main.c
+++ src/main.c
@@ -14,6 +14,10 @@ typedef struct
     GObject *battery_level;
     GObject *battery_level_str;
 
+    GtkComboBox *trigger;
+    GtkComboBox *sample_rate;
+    GtkComboBox *sample_depth;
+
     SooshiState *sooshi;
 } AppState;
 
@@ -54,12 +58,75 @@ void channel2_update(SooshiState *state, SooshiNode *node, gpointer user_data)
     g_free(str);
 }
 
+static void fill_combo_box_with_children(GtkComboBox *box, SooshiNode *node)
+{
+    GtkListStore *store = GTK_LIST_STORE(gtk_combo_box_get_model(box));
+
+    GList *elem;
+    SooshiNode *item;
+    gint id = 0;
+    for(elem = node->children; elem; elem = elem->next)
+    {
+        item = elem->data;
+
+        GtkTreeIter iter;
+        gtk_list_store_append(store, &iter);
+        gtk_list_store_set(store, &iter, 0, item->name, 1, id, -1);
+
+        id++;
+    }
+
+    gtk_combo_box_set_active_id(box, 0);
+}
+
+static void
+sample_config_update(SooshiState *state, SooshiNode *node, gpointer user_data)
+{
+    gtk_combo_box_set_active(GTK_COMBO_BOX(user_data), g_variant_get_byte(node->value));
+}
+
+static void
+sample_config_changed(GtkComboBox *box, gpointer user_data)
+{
+    AppState *s = (AppState*)user_data;
+
+    gint chosen_value = gtk_combo_box_get_active(box);
+
+    SooshiNode *base_node;
+    if (box == s->trigger)
+        base_node = sooshi_node_find(s->sooshi, "SAMPLING:TRIGGER", NULL);
+    else if (box == s->sample_rate)
+        base_node = sooshi_node_find(s->sooshi, "SAMPLING:RATE", NULL);
+    else if (box == s->sample_depth)
+        base_node = sooshi_node_find(s->sooshi, "SAMPLING:DEPTH", NULL);
+
+    sooshi_node_choose_by_index(s->sooshi, base_node, chosen_value);
+}
+
 void mooshi_initialized(SooshiState *state, gpointer user_data)
 {
+    AppState *s = (AppState*)user_data;
+
     g_info("Mooshimeter initialized!");
     sooshi_node_subscribe(state, sooshi_node_find(state, "BAT_V", NULL), battery_update, user_data);
     sooshi_node_subscribe(state, sooshi_node_find(state, "CH1:VALUE", NULL), channel1_update, user_data);
     sooshi_node_subscribe(state, sooshi_node_find(state, "CH2:VALUE", NULL), channel2_update, user_data);
+
+    SooshiNode *sampling_trigger    = sooshi_node_find(state, "SAMPLING:TRIGGER", NULL);
+    SooshiNode *sampling_rate       = sooshi_node_find(state, "SAMPLING:RATE", NULL);
+    SooshiNode *sampling_depth      = sooshi_node_find(state, "SAMPLING:DEPTH", NULL);
+
+    fill_combo_box_with_children(s->trigger, sampling_trigger);
+    fill_combo_box_with_children(s->sample_rate, sampling_rate);
+    fill_combo_box_with_children(s->sample_depth, sampling_depth);
+
+    sooshi_node_subscribe(state, sampling_trigger, sample_config_update, s->trigger);
+    sooshi_node_subscribe(state, sampling_rate, sample_config_update, s->sample_rate);
+    sooshi_node_subscribe(state, sampling_depth, sample_config_update, s->sample_depth);
+
+    g_signal_connect(s->trigger, "changed", G_CALLBACK(sample_config_changed), s);
+    g_signal_connect(s->sample_rate, "changed", G_CALLBACK(sample_config_changed), s);
+    g_signal_connect(s->sample_depth, "changed", G_CALLBACK(sample_config_changed), s);
 
     sooshi_node_choose(state, sooshi_node_find(state, "SAMPLING:TRIGGER:CONTINUOUS", NULL));
     sooshi_node_choose(state, sooshi_node_find(state, "CH2:MAPPING:VOLTAGE", NULL));
@@ -67,10 +134,13 @@ void mooshi_initialized(SooshiState *state, gpointer user_data)
     sooshi_node_set_value(state, sooshi_node_find(state, "CH2:RANGE_I", NULL), g_variant_new_byte(0), TRUE);
 }
 
+void mooshi_scan_timed_out(SooshiState *state, gpointer user_data)
+{
+
+}
+
 void app_shutdown(gpointer user_data)
 {
-    AppState *s = (AppState*)user_data;
-    sooshi_state_delete(s->sooshi);
     gtk_main_quit();
 }
 
@@ -91,23 +161,32 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    SooshiState *state = sooshi_state_new();
-    AppState s;
-    s.channel1_top    = gtk_builder_get_object(builder, "channel1_top");
-    s.channel1        = gtk_builder_get_object(builder, "channel1");
-    s.channel1_bottom = gtk_builder_get_object(builder, "channel1_bottom");
+    sooshi_error_t sooshi_error = 0;
+    SooshiState *state = sooshi_state_new(&sooshi_error);
 
-    s.channel2_top    = gtk_builder_get_object(builder, "channel2_top");
-    s.channel2        = gtk_builder_get_object(builder, "channel2");
-    s.channel2_bottom = gtk_builder_get_object(builder, "channel2_bottom");
+    if (sooshi_error)
+        return -1;
 
-    s.battery_level     = gtk_builder_get_object(builder, "battery_level");
-    s.battery_level_str = gtk_builder_get_object(builder, "battery_level_str");
+    AppState *s = g_new0(AppState, 1);
+    s->channel1_top    = gtk_builder_get_object(builder, "channel1_top");
+    s->channel1        = gtk_builder_get_object(builder, "channel1");
+    s->channel1_bottom = gtk_builder_get_object(builder, "channel1_bottom");
 
-    s.sooshi = state;
+    s->channel2_top    = gtk_builder_get_object(builder, "channel2_top");
+    s->channel2        = gtk_builder_get_object(builder, "channel2");
+    s->channel2_bottom = gtk_builder_get_object(builder, "channel2_bottom");
+
+    s->battery_level     = gtk_builder_get_object(builder, "battery_level");
+    s->battery_level_str = gtk_builder_get_object(builder, "battery_level_str");
+
+    s->trigger          = GTK_COMBO_BOX(gtk_builder_get_object(builder, "Trigger"));
+    s->sample_rate      = GTK_COMBO_BOX(gtk_builder_get_object(builder, "Sample Rate"));
+    s->sample_depth     = GTK_COMBO_BOX(gtk_builder_get_object(builder, "Sample Depth"));
+
+    s->sooshi = state;
 
     GObject *window = gtk_builder_get_object(builder, "MainWindow");
-    g_signal_connect(window, "destroy", G_CALLBACK(app_shutdown), &s);
+    g_signal_connect(window, "destroy", G_CALLBACK(app_shutdown), s);
 
     /* Set up CSS style provider */
     GtkCssProvider *provider = gtk_css_provider_new();
@@ -130,11 +209,16 @@ int main(int argc, char *argv[])
 
     g_object_unref(provider);
 
-    sooshi_setup(state, mooshi_initialized, &s);
+    sooshi_setup(state,
+            mooshi_initialized, s,
+            mooshi_scan_timed_out, s
+    );
 
     gtk_widget_show_all(GTK_WIDGET(window));
 
     gtk_main();
+
+    sooshi_state_delete(state);
 
     return 0;
 }
